28e058c5b6db9c07b51082311c134416908782c4
Christopher Cahill <cthomascahill@gmail.com>
2018-10-23 21:03:31 -0700

    initialize site repo

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+// colorChanger
+$(document).ready(function() {
+
+  $("form#colorForm").submit(function() {
+      var bgColor = $("#color").val();
+      $("body").css("background-color", bgColor);
+      event.preventDefault();
+    });
+
+    $("form#colorFontForm").submit(function() {
+      var fontColor = $("#fontColor").val();
+      $("body").css("color", fontColor);
+      event.preventDefault();
+    });
+
+});
+
+//textReplacer
+function splitString(stringToSplit, separator) {
+    return (stringToSplit.split(separator));
+}
+
+function letterDelete(inputArray, deleteArrayFunc) {
+  var returnArray = [];
+  for (var i = 0; i <= (inputArray.length)-1; i++) {
+    if (deleteArrayFunc.includes(inputArray[i])) {
+    returnArray.push("-")
+    }
+    else {returnArray.push(inputArray[i])
+    }
+  }
+  return returnArray;
+}
+
+$(document).ready(function() {
+  $("form#formSubmit").submit(function(event) {
+    event.preventDefault();
+    $("#inputStringHere").empty();
+    $("#removeLetters").empty();
+    $("#deletedStringHere").empty();
+    var inputString = $("#stringInput").val();
+    var deleteString = $("#letterInput").val();
+    var stringArray = splitString(inputString, "");
+    var deleteArray = splitString(deleteString, "")
+    var deletedLetters = letterDelete(stringArray, deleteArray);
+    $("#inputStringHere").append(stringArray);
+    $("#removeLetters").append(deleteArray);
+    $("#deletedStringHere").append(deletedLetters);
+    console.log(stringArray);
+    console.log(deleteArray);
+    console.log(deletedLetters);
+  });
+});
